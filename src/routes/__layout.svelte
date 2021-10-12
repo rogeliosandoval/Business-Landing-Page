b99fee9f4e6bef246917ffccc7b8c77f01b65321
Roger <rogelio.g.sandoval@gmail.com>
2021-10-12 15:00:59 -0500

    Initialize the project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Navbar from "$lib/components/navbar.svelte";
+	import Footer from "$lib/components/footer.svelte";
+</script>
+
+<main class="bg-yellow-700 m-0 w-screen h-screen">
+<Navbar />
+<slot />
+<Footer />
+</main>
