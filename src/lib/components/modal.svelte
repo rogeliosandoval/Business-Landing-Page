64d26c7dc485feecf73c89ddf2768b5e655e5ce9
Roger <rogelio.g.sandoval@gmail.com>
2021-10-26 21:02:04 -0500

    WIP modals

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -10,9 +10,30 @@
             Inspector
         </p>
 
-        <p class="text-gray-700 text-base">
-            Join our team of inspectors and help others find piece within their porperties. Our inspectors are what drive this business and we would love to have you on the team!
+        <p class="text-black text-sm pb-5 font-bold">
+            Join our team of inspectors and help others find peace within their porperties. Our inspectors are what drive this business and we would love to have you on the team! Our inspectors make everything possible.
         </p>
+
+        <div class="container grid grid-cols-2 gap-2 pb-5">
+
+            <div>
+                <video class="bigvid mx-auto" style="width:100%" muted autoplay loop>
+                    <source src="https://metrixhosting.com/wp-content/uploads/2020/08/Preview-yourtime-site-vids-1.mp4" type="video/mp4" />
+                  </video>
+            </div>
+
+            <div class="mx-auto">
+                <p class="font-bold pb-1">Some of the expectations</p>
+                <ul class="list-disc">
+                    <li>checking light fictures</li>
+                    <li>observing the plumbing</li>
+                    <li>checking foundation</li>
+                    <li>going on roofs</li>
+                    <li>writting up reports</li>
+                </ul>
+            </div>
+
+        </div>
 
         <button class="bg-red-500 hover:bg-red-400 text-black font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"on:click="{() => displayModal = false}">Close</button>
     </div>
@@ -26,7 +47,7 @@
             Client Coordinator
         </p>
 
-        <p class="text-gray-700 text-base">
+        <p class="text-gray-700 text-base pb-5">
             Our client coordinators play an essential role in getting our inspectors to you. Help book inspections, set up events, or simply explain what Your Time Home Inspections is all about with generalized information.
         </p>
 
@@ -54,4 +75,5 @@
         top: 25%;
         background-color: cornsilk
     }
+
 </style>
