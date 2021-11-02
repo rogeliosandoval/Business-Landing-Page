0e67cb2ed8e52b21e69b1dbc5652c44542932042
Roger <rogelio.g.sandoval@gmail.com>
2021-11-02 16:38:47 -0500

    Start the contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,1 +1,43 @@
-<h1>Contact Our Assess</h1>
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+<section class="relative pb-12 pt-12 bg-yellow-200 font-sans">
+
+    <div class="text-6xl font-bold text-black text-center pt-10 pb-7">
+        Contact Us!
+    </div>
+
+    <div class="container mx-auto pt-7 pb-7 text-center grid grid-cols-3 gap-x-20">
+        
+        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100">
+        <i class="pt-5 text-black fas fa-phone fa-7x"></i>
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-1">Phone</div>
+            <div class="text-xl mb-4">(210) 367-2945</div>
+            <div class="font-bold text-xl mb-1">Email</div>
+            <div class="text-xl">info@yourtimeinspections</div>
+        </div>
+        </div>
+
+        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100 pb-7">
+        <i class="pt-5 text-black fas fa-clock fa-7x"></i>
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-4">Our Hours</div>
+            <div class="text-xl">Monday-Friday</div>
+            <div class="text-xl mb-4">8:00am-4:00pm</div>
+            <div class="text-xl">Saturday</div>
+            <div class="text-xl mb-4">8:00am-4:00pm</div>
+            <div class="font-bold text-xl">Call For Exceptions!</div>
+        </div>
+        </div>
+
+        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100">
+        <i class="pt-5 text-black fas fa-map-pin fa-7x"></i>
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Location</div>
+        </div>
+        </div>
+
+    </div>
+
+</section>
