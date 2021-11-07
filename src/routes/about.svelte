ff6abe7f699db03c9b6fea52c34183836250895e
Roger <rogelio.g.sandoval@gmail.com>
2021-11-07 13:54:16 -0600

    Add more content to about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,16 +3,41 @@
 
 <section class="relative pb-12 pt-12 bg-yellow-200 font-sans">
 
-    <div class="text-6xl font-bold text-black text-center pt-10 pb-7">
+    <div class="text-6xl font-bold text-black text-center pt-7 pb-7">
         About Us!
     </div> 
 
-    <div class="container mx-auto pt-7 pb-7 text-center grid grid-cols-3 gap-x-20">
+    <div class="container mx-auto pt-7 pb-16 text-center grid grid-cols-2 gap-x-20">
         
-        <img class="mx-auto" alt="house" src="../../img/bighouse.webp">
+        <div>
+            <img class="mx-auto" style="width:400px;height:350px;" alt="house" src="../../img/bighouse.webp">
+        </div>
+
+        <div>
+            <p>The Right Choice</p>
+            <p class="font-bold text-4xl">Home Inspector In San Antonio, TX</p>
+            <br>
+            <p class="tracking-wide">Your Time Home Inspections prides itself on following a comprehensive standard of practice which ensures our clients will receive a detailed accurate home inspection. We abide by a strict code of ethics, which puts each client’s interest first</p>
+            <br>
+            <p class="tracking-wide">We are members of SABOR, TPREIA, as well as a certified professional inspector, trained and certified by InterNachi. Call or book your home inspection online today.</p>
+        </div>
 
     </div>
 
-</section>
+    <div class="px-80 py-12 mx-auto bg-yellow-600 grid grid-cols-2 gap-x-10">
 
-TESTING
+        <div class="px-9 py-8">
+            <p class="font-bold text-xl">Our Owners</p>
+            <p class="font-bold text-5xl text-white tracking-wide pt-5">James & Elvia Williams</p>
+            <p class="font-bold  text-white pt-5 text-xl">Our Promise To You</p>
+            <p class="tracking-widest text-white">Choosing the right home inspector can be difficult. Different inspectors have varying qualifications, equipment, experience, reporting methods, and pricing. One thing that’s certain is that a home inspection requires a lot of work. Ultimately, a thorough inspection depends heavily on the individual inspector’s effort. If you honor us with your trust to inspect your new home, we guarantee that we will give you a very best effort. This is our promise to you.</p>
+            <p class="font-bold text-white pt-5 text-xl">Giving Back</p>
+            <p class="tracking-widest text-white">We believe that true success is about more than growing a Home Inspection business; we devote time each year to charitable causes that benefit diverse populations. From helping the homeless to sponsoring charities that help families adopt children, Your Time Home Inspections is committed to improving the town we call home.</p>
+        </div>
+
+        <div>
+            <img class="mx-auto" style="border: 5px solid white" alt="owners" src="https://yourtimehomeinspections.com/wp-content/uploads/2020/08/yourtimeowners-768x768.jpg">
+        </div>
+    </div>
+
+</section>
