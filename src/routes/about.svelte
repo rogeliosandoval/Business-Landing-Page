9f4780036c3ef05edc9dd28754e9000148dd6e82
Roger <rogelio.g.sandoval@gmail.com>
2021-11-11 21:36:35 -0600

    Create more employees

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,31 +5,60 @@
     import Card from "$lib/components/card.svelte"
     
     const employees = [
-    { 
-        name: "Roger Sandoval",
-        image: "../../img/employees/roggie.jpg",
-        bio: "I am a Grandma to 4 boys and one girl and a Great Grandma to three girls and one boy. I have over 10 years in clerical and support staff experience. I am Blessed to be a part of Your Time Home Inspections family.",
-    },
-    {
-        name: "Rebekah Morehouse",
-        image: "../../img/employees/rebekah.jpg",
-        bio: "I have been working in the home inspection business for 3+ years, scheduling inspections and providing high quality customer service. I look forward to assisting in anyway possible and improving my skills to continue serving our clients.",
-    },
-    {
-        name: "Kalen Bahlman",
-        image: "../../img/employees/kalen.jpg",
-        bio: "Born and Raised in San Antonio Texas. Went to College in Virginia studied Political Science, Multimedia Journalism and creative writing. Has 5 years of Customer service and spare time collects everything Star Wars.",
-    },
-    {
-        name: "Belinda Burnett",
-        image: "../../img/employees/belinda.jpg",
-        bio: "I am a Grandma to 4 boys and one girl and a Great Grandma to three girls and one boy. I have over 10 years in clerical and support staff experience. I am Blessed to be a part of Your Time Home Inspections family",
-    },
-    {
-        name: "Elvia Williams",
-        image: "../../img/employees/elvia.jpg",
-        bio: "Wife & Mother of three amazing young men and a Sidney Lanier Graduate. Banker by trade & now proud Co-owner with my Husband James Williams of Your Time Home Inspections. We continue to be blessed beyond words."
-    }]
+    
+        {
+            name: "Rebekah Morehouse",
+            image: "../../img/employees/rebekah.jpg",
+            bio: "I have been working in the home inspection business for 3+ years, scheduling inspections and providing high quality customer service. I look forward to assisting in anyway possible and improving my skills to continue serving our clients.",
+        },
+        {
+            name: "Kalen Bahlman",
+            image: "../../img/employees/kalen.jpg",
+            bio: "Born and Raised in San Antonio Texas. Went to College in Virginia studied Political Science, Multimedia Journalism and creative writing. Has 5 years of Customer service and spare time collects everything Star Wars.",
+        },
+        {
+            name: "Belinda Burnett",
+            image: "../../img/employees/belinda.jpg",
+            bio: "I am a Grandma to 4 boys and one girl and a Great Grandma to three girls and one boy. I have over 10 years in clerical and support staff experience. I am Blessed to be a part of Your Time Home Inspections family",
+        },
+        {
+            name: "Elvia Williams",
+            image: "../../img/employees/elvia.jpg",
+            bio: "Wife & Mother of three amazing young men and a Sidney Lanier Graduate. Banker by trade & now proud Co-owner with my Husband James Williams of Your Time Home Inspections. We continue to be blessed beyond words."
+        },
+        { 
+            name: "Roger Sandoval",
+            image: "../../img/employees/roggie.jpg",
+            bio: "I am a Grandma to 4 boys and one girl and a Great Grandma to three girls and one boy. I have over 10 years in clerical and support staff experience. I am Blessed to be a part of Your Time Home Inspections family.",
+        }
+    ]
+
+    const employees2 = [
+        {
+            name: "James Williams",
+            image: "../../img/employees/james.jpg",
+            bio: "James is co-owner and lead inspector of Your Time Home Inspections. He has been in the construction field both in residential and commercial his whole career to include commercial construction and commercial concrete.",
+            trec: "21507"
+        },
+        {
+            name: "Luis Vargas",
+            image: "../../img/employees/luis.jpg",
+            bio: "I am very motivated and enthusiastic to help people when buying/selling a home. Married and father of an amazing woman. I speak Spanish and graduated in Mechanical Engineering.",
+            trec: "23535"
+        },
+        {
+            name: "Robbie Spencer",
+            image: "../../img/employees/robbie.jpg",
+            bio: "Proud Retired ARMY Staff Sergeant, Combat Veteran, and Patriot enlisting to follow in my father’s footsteps. I’m extremely proud to be a part of Your Time Home Inspections and eager to serve your home inspection needs!",
+            trec: "23968"
+        },
+        {
+            name: "Nicholas Pooley",
+            image: "../../img/employees/nicholas.jpg",
+            bio: "Owned his own Home Inspection Company in Maine called “ Bay view Home Inspections”. Has two cats one is 8 years old named Daryl Loves to play Bass on his spare time and Bartend.",
+            trec: "24758"
+        }
+    ]
 
 </script>
 
@@ -51,7 +80,16 @@
     width: 1150px;
     height: 424px;
     position: absolute;
-    top: 60%;
+    top: 53%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    }
+
+    .cent3 {
+    width: 1150px;
+    height: 424px;
+    position: absolute;
+    top: 73%;
     left: 50%;
     transform: translate(-50%, -50%);
     }
@@ -122,12 +160,12 @@
         
         <div class="cent2 container mx-auto text-center">
 
-            <div class="text-white tracking-wider">
+            <div class="text-white tracking-wider text-lg">
                 Your Time Home Inspections
             </div>
             
             <div class="pb-40 text-white font-bold">
-                <p class="font-bold text-6xl">Our Support Team</p>
+                <p class="font-bold text-6xl tracking-wider">Our Support Team</p>
             </div>
 
         </div>
@@ -136,6 +174,32 @@
     <section class="mt-10 flex flex-wrap mx-auto w-3/4 gap-16 justify-center">
 
         {#each employees as employee}
+            <Card employee={employee} />
+        {/each}
+
+    </section>
+
+    <div class="mx-auto pt-10">
+        <video class="bigvid mx-auto border-t-4 border-b-4 border-blue-500 object-cover h-96 w-full" style="width:100%" muted autoplay loop>
+        <source src="https://metrixhosting.com/wp-content/uploads/2020/08/Preview-yourtime-site-vids-1.mp4" type="video/mp4" />
+        </video>
+        
+        <div class="cent3 container mx-auto text-center">
+
+            <div class="text-white tracking-wider text-lg">
+                Your Time Home Inspections
+            </div>
+            
+            <div class="pb-40 text-white font-bold">
+                <p class="font-bold text-6xl tracking-wider">Your Home Inspectors</p>
+            </div>
+
+        </div>
+    </div>
+
+    <section class="mt-10 flex flex-wrap mx-auto w-3/4 gap-16 justify-center">
+
+        {#each employees2 as employee}
             <Card employee={employee} />
         {/each}
 
