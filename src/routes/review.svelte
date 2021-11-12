fad916ac10e0e27ab64e021c8578ffa8489676f5
Roger <rogelio.g.sandoval@gmail.com>
2021-11-12 14:18:19 -0600

    WIP

--- src/routes/review.svelte
+++ src/routes/review.svelte
@@ -4,33 +4,8 @@
 <section class="relative pb-12 pt-12 bg-yellow-200 font-sans">
 
     <div class="text-6xl font-bold text-black text-center pt-10 pb-7">
-        Inspected Once, Inspected Right®
+        Our Reviews!
     </div> 
-
-    <div class="container mx-auto pt-7 pb-7 text-center grid grid-cols-3 gap-x-20">
-        
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100">
-        <i class="pt-5 text-black fas fa-tags fa-7x"></i>
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Pre-Listing</div>
-        </div>
-        </div>
-
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100 pb-7">
-        <i class="pt-5 text-black fas fa-home fa-7x"></i>
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Residential</div>
-        </div>
-        </div>
-
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-100">
-        <i class="pt-5 text-black fas fa-money fa-7x"></i>
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Investments</div>
-        </div>
-        </div>
-
-    </div>
 
 </section>
 
